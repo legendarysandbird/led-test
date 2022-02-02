88a03c1ffb753afe2fa4e342b3ef44b1f44a3b9d
legendarysandbird <legendarysandbird@gmail.com>
2022-02-02 13:45:53 -0800

    Make run switch to nightly branch

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+rustup override set nightly
 export AVR_CPU_FREQUENCY_HZ=16000000
 
 cargo build -Z build-std=core --target avr-atmega328p.json --release && avrdude -patmega328p -carduino -P/dev/ttyACM0 -b115200 -D -Uflash:w:target/avr-atmega328p/release/blink.elf:e
