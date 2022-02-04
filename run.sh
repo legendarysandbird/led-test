a2db84eb1b8ee1c865951f51c8903011d569712a
legendarysandbird <legendarysandbird@gmail.com>
2022-02-04 10:39:53 -0800

    Now change colors with a button

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-rustup override set nightly
+rustup override set nightly-2021-01-07
+rustup component add rust-src
 export AVR_CPU_FREQUENCY_HZ=16000000
 
 cargo build -Z build-std=core --target avr-atmega328p.json --release && avrdude -patmega328p -carduino -P/dev/ttyACM0 -b115200 -D -Uflash:w:target/avr-atmega328p/release/blink.elf:e
--- src/main.rs
+++ src/main.rs
@@ -6,26 +6,54 @@
 use ruduino::Pin;
 use ruduino::cores::current::{port};
 
+enum Color {
+    Red,
+    Green,
+    Blue,
+}
+
 #[no_mangle]
 pub extern fn main() {
     port::D6::set_output();
     port::D5::set_output();
     port::D3::set_output();
 
+    port::B5::set_input();
+
+    let mut just_pressed = false;
+    let mut color: Color = Color::Red;
+
+    port::D3::set_low();
+    port::D6::set_high();
+
     loop {
-        port::D3::set_low();
-        port::D6::set_high();
 
-        ruduino::delay::delay_ms(1000);
+        if port::B5::is_low() && !just_pressed {
+            just_pressed = true;
+        } else if port::B5::is_high() {
+            just_pressed = false;
+        }
 
-        port::D6::set_low();
-        port::D5::set_high();
 
-        ruduino::delay::delay_ms(1000);
-
-        port::D5::set_low();
-        port::D3::set_high();
-
-        ruduino::delay::delay_ms(1000);
+        if just_pressed {
+            match color {
+                Color::Red => {
+                    color = Color::Green;
+                    port::D6::set_low();
+                    port::D5::set_high();
+                },
+                Color::Green => {
+                    color = Color::Blue;
+                    port::D5::set_low();
+                    port::D3::set_high();
+                },
+                Color::Blue => {
+                    color = Color::Red;
+                    port::D3::set_low();
+                    port::D6::set_high();
+                },
+            }
+            ruduino::delay::delay_ms(500);
+        }
     }
 }
